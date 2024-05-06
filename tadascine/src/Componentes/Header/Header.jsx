c42788c17cf38eab98347e1dc5b886739633b6cc
VictorTadashi <victortadashisaitobarra@gmail.com>
2024-05-06 12:52:56 -0300

    Aula 1 - cinema

---
+++ tadascine/src/Componentes/Header/Header.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../Navbar/Navbar";
+
+
+function Header() {
+    return (  
+        <header>
+            <span>Olá, visitantes</span>
+            <h1>Bem-vindo ao Tadascine</h1>
+            <Navbar/>
+        </header>
+    );
+}
+
+export default Header;
