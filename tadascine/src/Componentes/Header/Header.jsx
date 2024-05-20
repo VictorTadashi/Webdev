a6b5b30136a8e4f719d4b28918e34166cafc4719
VictorTadashi <victortadashisaitobarra@gmail.com>
2024-05-20 12:55:45 -0300

    Aula 3 - Cinema

--- tadascine/src/Componentes/Header/Header.jsx
+++ tadascine/src/Componentes/Header/Header.jsx
@@ -3,9 +3,9 @@ import Navbar from "../Navbar/Navbar";
 
 function Header() {
     return (  
-        <header className="flex">
+        <header className="flex justify-around bg-primary-raul text-slate-50 p-4">
             <span>Olá, visitantes</span>
-            <h1>Bem-vindo ao Tadascine!!</h1>
+            <h1>Bem-vindo ao Tadascine!!!</h1>
             <Navbar/>
         </header>
     );
