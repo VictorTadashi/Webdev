0bb5fa84442659385fed3052ab831cd032e79f6a
VictorTadashi <victortadashisaitobarra@gmail.com>
2024-05-27 12:54:30 -0300

    Aula final - ac1

--- tadascine/src/pages/Home.jsx
+++ tadascine/src/pages/Home.jsx
@@ -7,11 +7,29 @@ function Home() {
         <main>
             <input type="text" name="busca" id="busca" placeholder="Digite aqui sua busca:" />
             <div className="lista-filmes">
+                <div className='grid grid-cols-3 gap-4'>
+
                 {data.map(filme => (
-                    <h1>{filme.title}</h1>
+                    <div className='card' key = {filme.title}>
+                     <h1>{filme.title}</h1>
+                     <img src={filme.image} alt={filme.title}/>
+                     <div className='tag bg-primary-raul'>
+                        {filme.tags.map(tag => (
+                            <span key={tag}> {tag} </span>
+                        ))}
+                     </div>
+                     <div className='text bg-gray-500'>
+                        {filme.tags.map(text => (
+                            <span key={text}> {text} </span>
+                        ))}
+                     </div>
+                    </div>
+
+                    
                 )
                 )
                 }
+                </div>
             </div>
         </main>
         </>
