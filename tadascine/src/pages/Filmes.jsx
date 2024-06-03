3f65e6773f9b637c8d1cec4106e9231740438856
VictorTadashi <victortadashisaitobarra@gmail.com>
2024-06-03 12:58:30 -0300

    Aula 4

--- tadascine/src/pages/Filmes.jsx
+++ tadascine/src/pages/Filmes.jsx
@@ -1,6 +1,33 @@
+import { useState } from "react"
+
 function Filmes() {
+
+
+    const [filmes, setFilmes] = useState([])
+
+    const apiKey='api_key=7c572a9f5b3ba776080330d23bb76e1e'
+    const urlBase = 'https://api.themoviedb.org/3/movie/'
+    const urlImg = 'https://image.tmdb.org/t/p/w342/'
+
+
+    fetch('https://api.themoviedb.org/3/movie/popular?api_key=eb3b2297dfc1a7fdee0f68a2993d57b4')
+    .then(response => response.json())
+    .then(response => setFilmes(response.results))
+    .catch(erro => console.log(erro))
+
     return (  
+        <>
         <h1>Filmes</h1>
+        {
+            filmes.map(filme =>(
+                <div className="card-filme" key {filme.id}>
+                    <h1>{filme.title}</h1>
+                </div>
+                
+            ))
+        }
+        
+        </>
     );
 }
 
